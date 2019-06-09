b5a87c918803f80426abddbb6fd704745efe4ef6
corey chang <coreychang808@Hotmail.com>
2019-06-09 14:17:09 -0700

    branch 5

--- app.js
+++ app.js
@@ -85,14 +85,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
-
 function sumArray(sumArr) { //eslint-disable-line
-
+  var arraySum = sum(testArray[0],testArray[1])[0];
+  arraySum = sum(testArray[2],arraySum)[0];
+  return [arraySum, testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + arraySum + ' is their sum.'];
 }
+console.log(sumArray(testArray));
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -107,16 +109,21 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
+var testArray1 = [2,3,4]; //eslint-disable-line
 function multiplyArray(multArr) { //eslint-disable-line
-
+  var arrayMult = multiply(testArray1[0],testArray1[1])[0];
+  arrayMult = multiply(testArray1[2],arrayMult)[0];
+  return [arrayMult, 'The numbers ' + testArray1[0] + ',' + testArray1[1] + ',' + testArray1[2] + ' have a product of ' + arrayMult + '.'];
 }
+console.log(multiplyArray(testArray1));
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray1);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
+
 
 // Don't forget to create a new branch for your work on the next question, if you attempt it.
 
